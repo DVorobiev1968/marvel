823a09c9bbf7c67ad527b1535c80c54b5fe59890
DVorobiev1968 <vdv@rdturbo.ru>
2023-04-19 09:32:35 +0500

    Добавил анализ props

--- src/components/charInfo/CharInfo.js
+++ src/components/charInfo/CharInfo.js
@@ -1,4 +1,5 @@
 import { Component } from "react";
+import { PropTypes } from "prop-types";
 import Spinner from "../spinner/Spinner";
 import ErrorMessage from "../errorMessage/ErrorMessage";
 import Skeleton from "../skeleton/Skeleton";
@@ -121,4 +122,10 @@ const View = ({ char }) => {
     </>
   );
 };
+
+CharInfo.propTypes={
+  charId:PropTypes.number,
+  
+}
+
 export default CharInfo;
